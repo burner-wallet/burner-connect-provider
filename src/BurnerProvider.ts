8d57da4ba2a29081e7250fc143dde1a1a1fea068
David Mihal <david@dmihal.com>
2020-01-29 12:26:42 -0500

    Compliant with eip-1193

--- src/BurnerProvider.ts
+++ src/BurnerProvider.ts
@@ -1,22 +1,26 @@
+import { EventEmitter } from 'events';
 import HubBridge from './HubBridge';
 import WalletBridge from './WalletBridge';
 import showWalletSelector from './WalletSelector';
 
 const id = () => (Math.random() * 100000000) | 0;
 
-export default class BurnerProvider {
+export default class BurnerProvider extends EventEmitter {
   public hub: HubBridge;
   public connected = false;
   public walletBridge: WalletBridge | null = null;
   public wallet: any = null;
   public network = '1';
+  private _nextJsonRpcId = 0;
 
   constructor(hubUrl = 'https://burnerconnect.xyz/') {
+    super();
     this.hub = new HubBridge(hubUrl);
   }
 
   setNetwork(network: string) {
     this.network = network;
+    this.emit('chainChanged', network);
   }
 
   enable() {
@@ -26,12 +30,15 @@ export default class BurnerProvider {
 
     return this.hub.getWallets()
       .then((wallets: any[]) => this.showPrompt(wallets))
-      .then(() => this.walletBridge!.send({
-        id: id(),
-        method: 'eth_accounts',
-        network: this.network,
-      }))
-      .then(() => { this.connected = true })
+      .then(async () => {
+        const accounts = await this.walletBridge!.send({
+          id: id(),
+          method: 'eth_accounts',
+          network: this.network,
+        });
+        this.connected = true;
+        this.emit('connect');
+      })
       .catch(() => null);
   }
 
@@ -43,9 +50,22 @@ export default class BurnerProvider {
       });
   }
 
-  send() {
-    throw new Error('send not supported');
+  send(method: string, params: any[] = []) {
+    if (!method || typeof method !== 'string') {
+      return new Error('Method is not a valid string.');
+    }
+
+    if (!(params instanceof Array)) {
+      return new Error('Params is not a valid array.');
+    }
+
+    const id = this._nextJsonRpcId++;
+    const payload = { jsonrpc: '2.0', id, method, params };
+
+    return this.walletBridge!.send({ network: this.network, ...payload });
   }
+
+
 
   sendAsync(payload: any, cb: any) {
     if (!this.connected) {
